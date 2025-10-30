c4bd716e1bb19f38f2defab690e9563a7991c3f6
SimonLoir <simon@pixeldrive.be>
2025-10-30 17:43:05 +0100

    initial code formatting

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,8 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import sonarjs from 'eslint-plugin-sonarjs';
+import tseslint from 'typescript-eslint';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -10,23 +10,87 @@ import onlyWarn from "eslint-plugin-only-warn";
  * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      turbo: turboPlugin,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            turbo: turboPlugin,
+        },
+        rules: {
+            'turbo/no-undeclared-env-vars': 'off',
+        },
     },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+    {
+        plugins: { sonarjs },
+        rules: {
+            'sonarjs/no-duplicate-string': 'off',
+            'sonarjs/cognitive-complexity': 'warn',
+            'sonarjs/no-small-switch': 'warn',
+        },
     },
-  },
-  {
-    plugins: {
-      onlyWarn,
+
+    {
+        ignores: ['dist/**', '.next/**', '**/eslint.config.*'],
     },
-  },
-  {
-    ignores: ["dist/**"],
-  },
+    {
+        rules: {
+            'no-var': 'error',
+            'prefer-const': 'error',
+            'func-call-spacing': ['error', 'never'],
+            'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+            'no-debugger': 'error',
+            'no-dupe-keys': 'error',
+            'no-with': 'error',
+            eqeqeq: ['error', 'always'],
+            quotes: ['error', 'single', { avoidEscape: true }],
+            'no-unused-vars': 'off',
+            camelcase: 'error',
+            'no-duplicate-imports': 'error',
+            'require-await': 'error',
+            'no-undef': 'off',
+        },
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                allowDefaultProject: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            'require-await': 'error',
+            '@typescript-eslint/prefer-nullish-coalescing': 'error',
+            '@typescript-eslint/no-unused-expressions': 'error',
+            '@typescript-eslint/no-floating-promises': 'error',
+            '@typescript-eslint/no-misused-promises': [
+                'error',
+                { checksVoidReturn: false },
+            ],
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/array-type': 'error',
+            '@typescript-eslint/ban-ts-comment': [
+                'error',
+                {
+                    'ts-expect-error': 'allow-with-description',
+                    'ts-ignore': 'allow-with-description',
+                    'ts-nocheck': 'allow-with-description',
+                    'ts-check': false,
+                },
+            ],
+            '@typescript-eslint/no-require-imports': 'error',
+            '@typescript-eslint/prefer-as-const': 'error',
+            '@typescript-eslint/no-this-alias': 'error',
+            '@typescript-eslint/prefer-for-of': 'error',
+            '@typescript-eslint/no-empty-object-type': 'error',
+            '@typescript-eslint/no-unsafe-function-type': 'error',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+            '@typescript-eslint/no-wrapper-object-types': 'error',
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
+    },
 ];
