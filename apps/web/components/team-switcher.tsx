e5e985c2e9c831e4a65a717724a0ecaef622fab7
Simon Loir <simon-loir@hotmail.com>
2025-10-31 00:49:16 +0100

    feat: add organizations and apiKeys plugins in better auth

--- apps/web/components/team-switcher.tsx
+++ apps/web/components/team-switcher.tsx
@@ -18,18 +18,12 @@ import {
     SidebarMenuItem,
     useSidebar,
 } from '@/components/ui/sidebar';
+import { authClient } from '@/lib/auth-client';
 
-export function TeamSwitcher({
-    teams,
-}: {
-    teams: {
-        name: string;
-        logo: React.ElementType;
-        plan: string;
-    }[];
-}) {
+export function TeamSwitcher() {
+    const { data: organizations } = authClient.useListOrganizations();
     const { isMobile } = useSidebar();
-    const [activeTeam, setActiveTeam] = React.useState(teams[0]);
+    const [activeTeam, setActiveTeam] = React.useState(organizations?.[0]);
 
     if (!activeTeam) {
         return null;
@@ -44,15 +38,13 @@ export function TeamSwitcher({
                             size='lg'
                             className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
                         >
-                            <div className='bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
-                                <activeTeam.logo className='size-4' />
-                            </div>
+                            <div className='bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'></div>
                             <div className='grid flex-1 text-left text-sm leading-tight'>
                                 <span className='truncate font-medium'>
                                     {activeTeam.name}
                                 </span>
                                 <span className='truncate text-xs'>
-                                    {activeTeam.plan}
+                                    {activeTeam.id}
                                 </span>
                             </div>
                             <ChevronsUpDown className='ml-auto' />
@@ -67,15 +59,13 @@ export function TeamSwitcher({
                         <DropdownMenuLabel className='text-muted-foreground text-xs'>
                             Teams
                         </DropdownMenuLabel>
-                        {teams.map((team, index) => (
+                        {organizations?.map((team, index) => (
                             <DropdownMenuItem
                                 key={team.name}
                                 onClick={() => setActiveTeam(team)}
                                 className='gap-2 p-2'
                             >
-                                <div className='flex size-6 items-center justify-center rounded-md border'>
-                                    <team.logo className='size-3.5 shrink-0' />
-                                </div>
+                                <div className='flex size-6 items-center justify-center rounded-md border'></div>
                                 {team.name}
                                 <DropdownMenuShortcut>
                                     ⌘{index + 1}
