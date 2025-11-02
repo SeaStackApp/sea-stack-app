98b4c3f49db2002678a6234679443dee8b5fa689
Simon Loir <simon-loir@hotmail.com>
2025-11-02 15:19:56 +0100

    fix: fix build issues

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['@repo/api', '@repo/db'],
+    serverExternalPackages: ['ssh2'],
 };
 
 export default nextConfig;
