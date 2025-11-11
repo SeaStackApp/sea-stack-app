b021823fed28f8adb994ef525f23263992417c3c
Simon Loir <simon-loir@hotmail.com>
2025-11-11 01:23:57 +0100

    refactor: remove unused imports in services router
    - Removed `protectedProcedure`, `getSSHClient`, and `Docker` imports.
    - Simplified the services router with necessary dependencies only.

--- packages/api/src/routers/services/index.ts
+++ packages/api/src/routers/services/index.ts
@@ -1,11 +1,9 @@
-import { protectedProcedure, router } from '../../trpc';
+import { router } from '../../trpc';
 import { createSwarmService } from './createSwarmService';
 import { listServices } from './listServices';
 import { getService } from './getService';
 import { createDomain } from './createDomain';
 import { deleteService } from './deleteService';
-import { getSSHClient } from '../../utils/getSSHClient';
-import Docker from '../../utils/docker/Docker';
 import { deployService } from './deployService';
 
 export const servicesRouter = router({
