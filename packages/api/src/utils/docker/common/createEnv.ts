7904fdafedff91e3abccb3be4a11824b9f9a12d1
Simon Loir <simon-loir@hotmail.com>
2025-11-19 01:52:55 +0100

    feat: add utilities for handling environment variables and volumes in Docker services
    - Introduced `createEnv` and `createEnvFromString` utilities to manage environment variables for Docker containers.
    - Added decryption of environment variables in `deployService`.
    - Enhanced `deploySwarmService` with support for environment variables and volumes, including logging and mounting configurations.
    - Updated dependencies to include `@dotenvx/dotenvx` for environment variable parsing.

---
+++ packages/api/src/utils/docker/common/createEnv.ts
@@ -0,0 +1,9 @@
+import * as dotenvx from '@dotenvx/dotenvx';
+export const createEnv = (env: Record<string, string>) => {
+    return {
+        Env: Object.entries(env).map(([key, value]) => `${key}=${value}`),
+    };
+};
+
+export const createEnvFromString = (envString: string) =>
+    createEnv(dotenvx.parse(envString));
