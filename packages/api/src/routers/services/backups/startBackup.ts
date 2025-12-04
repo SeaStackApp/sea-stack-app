a0640447133126489b9976bf6fe01974b1758669
Simon Loir <simon-loir@hotmail.com>
2025-12-04 21:48:03 +0100

    feat: enhance `startBackup` procedure with validation and improved error handling
    - Added schedule existence validation and `TRPCError` for missing schedules.
    - Implemented service ownership checks using `checkServiceExistsInOrganization`.
    - Enhanced error handling with descriptive messages for queue creation failures.

--- packages/api/src/routers/services/backups/startBackup.ts
+++ packages/api/src/routers/services/backups/startBackup.ts
@@ -1,18 +1,53 @@
 import { protectedProcedure } from '../../../trpc';
 import { volumeBackupScheduleIdSchema } from '@repo/schemas';
 import { volumeBackupsQueue } from '@repo/queues';
+import { TRPCError } from '@trpc/server';
+import { checkServiceExistsInOrganization } from '@repo/utils';
 
 export const startBackup = protectedProcedure
     .input(volumeBackupScheduleIdSchema)
-    .mutation(async ({ ctx, input: { volumeBackupScheduleId } }) => {
-        const job = await volumeBackupsQueue.add(
-            `volume-backup-${volumeBackupScheduleId}`,
-            {
-                schedule: volumeBackupScheduleId,
+    .mutation(
+        async ({
+            ctx: { prisma, organizationId },
+            input: { volumeBackupScheduleId },
+        }) => {
+            const schedule = await prisma.volumeBackupSchedule.findUnique({
+                where: { id: volumeBackupScheduleId },
+                select: {
+                    volume: {
+                        select: {
+                            serviceId: true,
+                        },
+                    },
+                },
+            });
+
+            if (!schedule) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Volume backup schedule not found',
+                });
             }
-        );
 
-        console.log(job.id);
+            await checkServiceExistsInOrganization(
+                prisma,
+                schedule.volume.serviceId,
+                organizationId
+            );
 
-        return job;
-    });
+            try {
+                return await volumeBackupsQueue.add(
+                    `volume-backup-${volumeBackupScheduleId}`,
+                    {
+                        schedule: volumeBackupScheduleId,
+                    }
+                );
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: 'INTERNAL_SERVER_ERROR',
+                    message: 'Unable to create the backup task',
+                });
+            }
+        }
+    );
